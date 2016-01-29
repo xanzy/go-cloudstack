ec1674c58bdd35f51e05138371c4b8db92d13a60
Carles Figuerola <Carles.Figuerola@orbitz.com>
2016-01-29 09:32:55 -0600

    Add a special use case for CreateNetworkOffering

--- generate/generate.go
+++ generate/generate.go
@@ -973,7 +973,7 @@ func (s *service) generateNewAPICallFunc(a *API) {
 	pn("	}")
 	pn("")
 	switch n {
-	case "CreateNetwork", "CreateServiceOffering", "CreateSSHKeyPair", "RegisterSSHKeyPair":
+	case "CreateNetwork", "CreateNetworkOffering", "CreateServiceOffering", "CreateSSHKeyPair", "RegisterSSHKeyPair":
 		pn("	if resp, err = getRawValue(resp); err != nil {")
 		pn("		return nil, err")
 		pn("	}")
